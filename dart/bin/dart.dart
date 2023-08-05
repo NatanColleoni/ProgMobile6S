ead6beee8bec0e0f1ce97ab69034b9ac50f1ccff
Natan Fonseca <natancolleonifonseca@gmail.com>
2023-08-05 01:16:40 +0000

    04/08/2023 - Aula 2 - const, final, list, set, tipos, map - equivalente a dicionários em c#, spread

--- dart/bin/dart.dart
+++ dart/bin/dart.dart
@@ -1,10 +1,11 @@
 import 'package:dart/dart.dart' as dart;
 
-late String nome; // será inicializada em outro momento
+//late String nome; // será inicializada em outro momento
 
 void main(List<String> arguments) {
   
-  //04-08-2023 variaveis, nullable/non-nullable, late
+  // 04/08/2023 - Aula 1 -  variaveis, nullable/non-nullable, late
+  // 04/08/2023 - Aula 2 - const, final, list, set, tipos, map - equivalente a dicionários em c#, spread
   /*
   var nome = 'Natan';
   /*
@@ -29,10 +30,34 @@ void main(List<String> arguments) {
   print(nome);
   */
 
-  nome = _getNome();
-  print(nome);
-}
+  // nome = _getNome();
+  // print(nome);
 
+  // const listaCompras = ['Arroz', 'Feijão']; 
+  //listaCompras.add('Carne');  erro
+
+  // final listaItens = ['Monitor', 'Teclado']; // pode mudar no runtime
+  // listaItens.add('Computador');
+  // listaItens.removeWhere((element) => element == 'Computador');
+
+  // print(listaCompras);
+  // print(listaItens);
+  // Set<tipo> é um tipo com elementos que não se repetem
+  // var nome = 'Natan';
+  // print("Meu nome é: ${nome.toUpperCase()}");
+
+  // Map<String, int> salas = {
+  //   'Henrique': 13,
+  //   'João': 14
+  // };
+
+  // var listaCompra = ['Arroz', 'Feijão'];
+  // var novaLista = ['Macarrão', ...listaCompra];
+
+  // print(novaLista);
+}
+/*
 String _getNome() {
   return "";
 }
+*/
